bb64a6d20c908c115dd18fe9c1611e754d51dc5f
Andrew Zimmer <andrewzimmer906@gmail.com>
2011-10-26 15:15:02 -0600

    Fixed Readme and reachability

--- BlockUIImageLoader/URLImageCached.m
+++ BlockUIImageLoader/URLImageCached.m
@@ -20,6 +20,7 @@
  */
 typedef const struct __SCNetworkReachability * SCNetworkReachabilityRef;
 
+
 /* Private Class Methods */
 @interface URLImageCached(Private)
     +(BOOL)hasConnectivity;
@@ -54,7 +55,8 @@ typedef const struct __SCNetworkReachability * SCNetworkReachabilityRef;
 
 @implementation URLImageCached(Private)
 
-/* Connectivity testing code pulled from Apple's Reachability Example: http://developer.apple.com/library/ios/#samplecode/Reachability
+/* 
+    Connectivity testing code pulled from Apple's Reachability Example: http://developer.apple.com/library/ios/#samplecode/Reachability
  */
 +(BOOL)hasConnectivity {
     struct sockaddr_in zeroAddress;
@@ -63,8 +65,47 @@ typedef const struct __SCNetworkReachability * SCNetworkReachabilityRef;
 	zeroAddress.sin_family = AF_INET;
     
     SCNetworkReachabilityRef reachability = SCNetworkReachabilityCreateWithAddress(kCFAllocatorDefault, (const struct sockaddr*)&zeroAddress);
+    if(reachability != NULL) {
+        //NetworkStatus retVal = NotReachable;
+        SCNetworkReachabilityFlags flags;
+        if (SCNetworkReachabilityGetFlags(reachability, &flags)) {
+            if ((flags & kSCNetworkReachabilityFlagsReachable) == 0)
+            {
+                // if target host is not reachable
+                return NO;
+            }
+            
+            if ((flags & kSCNetworkReachabilityFlagsConnectionRequired) == 0)
+            {
+                // if target host is reachable and no connection is required
+                //  then we'll assume (for now) that your on Wi-Fi
+                return YES;
+            }
+            
+            
+            if ((((flags & kSCNetworkReachabilityFlagsConnectionOnDemand ) != 0) ||
+                 (flags & kSCNetworkReachabilityFlagsConnectionOnTraffic) != 0))
+            {
+                // ... and the connection is on-demand (or on-traffic) if the
+                //     calling application is using the CFSocketStream or higher APIs
+                
+                if ((flags & kSCNetworkReachabilityFlagsInterventionRequired) == 0)
+                {
+                    // ... and no [user] intervention is needed
+                    return YES;
+                }
+            }
+            
+            if ((flags & kSCNetworkReachabilityFlagsIsWWAN) == kSCNetworkReachabilityFlagsIsWWAN)
+            {
+                // ... but WWAN connections are OK if the calling application
+                //     is using the CFNetwork (CFSocketStream?) APIs.
+                return YES;
+            }
+        }
+    }
     
-    return reachability != NULL;
+    return NO;
 }
 
 @end
---
+++ README.markdown
@@ -0,0 +1,47 @@
+Block Image Loader
+=====
+
+Load a UIImage from a remote URL location in just a few lines of code.
+EXTRA BONUS: We are using blocks for our handlers!
+----------------------------------------------------------------------
+
+I created these two classes recently to enable myself to easily and efficiently load UIImage's from a remote URL asynchronously.
+	I set up blocks to manage the handlers, oh yeah! 
+
+* Asynchronously load UIImage's with just a few lines of code.
+* This is NOT a category on the UIImageView, so its much more flexible.
+* You get to do things in blocks! No more delegates!
+* Built in progress handler.
+* Built in reachability testing.
+
+**How to use**  
+First, download the code, and include it in your source.
+
+Import the class:
+     #import "URLImageCached.h"
+     
+Now, start loading images
+    -(void)loadImage {
+	    BOOL loading = [URLImageCached loadImageWithURL:@"http://imageurl.png" 
+	    					   withLoadCompleteHandler:^void(UIImage *loadedImage, NSString *url) {
+	    					       //Handle loaded image here. Set it to a UIImageView. Whatever..
+	    					   }];
+	    if(!loading) { NSLog(@"You'll need internet for this."); }
+    }
+    
+It's just as easy to include a progress indicator:
+    -(void)loadImageWithProgress {
+		BOOL loading = [URLImageCached loadImageWithURL:@"http://imageurl.png" 
+		                       withLoadProgressHandler:^void(float progress, NSString *url) {
+		                           //Handle the progress here. It'll go from 0.0 to 1.0.		    
+		                       }
+							   withLoadCompleteHandler:^void(UIImage *loadedImage, NSString *url) {
+							       //Handle loaded image here. Set it to a UIImageView. Whatever..
+							   }];
+		if(!loading) { NSLog(@"You'll need internet for this."); }
+    }
+    
+
+**Questions or Comments:**   
+Email: andrewzimmer906@gmail.com  
+Twitter: @andrewzimmer906  
